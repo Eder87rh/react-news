1f86079374c52c97c1bbf51b545cac87d4dd881c
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-12-01 12:43:22 -0600

    gettinf API data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {  }
+  state = {
+    noticias: []
+   }
+
+  async componentDidMount() {
+    this.consultarNoticias();
+  }
+  
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=6099896616084c37b615978f83f2961d`;
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+  
+    this.setState({ 
+      noticias: noticias.articles 
+    })
+  }
+
   render() { 
     return ( <h1> Noticias api react</h1> );
   }
